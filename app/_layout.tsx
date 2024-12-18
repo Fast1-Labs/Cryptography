1242b5444d69637a3c2ce887c584b564c7762fb1
anilyavas <anil.yavas99@gmail.com>
2024-12-18 15:21:00 +0300

    TabBarIcon and colors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,38 @@
+import { FontAwesome } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
+
+import { colors } from '~/constants/colors';
 
 export default function Layout() {
   return (
-    <Tabs screenOptions={{ headerShown: false, tabBarStyle: { backgroundColor: '#121212' } }}>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="trading" />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { backgroundColor: '#121212' },
+        tabBarActiveTintColor: colors.primary.main,
+        tabBarInactiveTintColor: 'white',
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome name="home" size={25} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="trading"
+        options={{
+          title: 'Trading',
+          tabBarIcon: ({ color }) => <FontAwesome name="money" size={25} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <FontAwesome name="user" size={25} color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
