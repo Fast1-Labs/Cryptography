0c104355a3aa63054ebe393e727c4ee97bc59253
anilyavas <anil.yavas99@gmail.com>
2025-01-03 18:02:28 +0300

    Search Container UI and function imp

--- app/(home)/investments.tsx
+++ app/(home)/investments.tsx
@@ -1,15 +1,36 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet, SafeAreaView, ActivityIndicator, Button } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 import { colors } from '~/constants/colors';
+import { useCoinStore } from '~/store/store';
 
 export default function InvestmentsScreen() {
+  const { coins, loading, error, fetchCoins } = useCoinStore();
+  const [search, setSearch] = useState('');
+
+  const handleSearch = async (search: string) => {};
+
+  if (loading) return <ActivityIndicator />;
+  if (error) return <Text style={{ color: 'red', fontSize: 20, fontWeight: 'bold' }}>{error}</Text>;
+
   return (
     <View style={styles.container}>
       <LinearGradient style={styles.container} colors={['#3E1D92', '#1B1030', '#000000']}>
-        {/* Header Title */}
         <SafeAreaView>
+          {/* Header Title */}
           <Text style={styles.headerTitle}>My Invesments</Text>
+          {/* Coin add search */}
+          <View style={styles.coinSearchContainer}>
+            <TextInput
+              placeholder="Search for coins..."
+              value={search}
+              onChangeText={setSearch}
+              style={styles.input}
+            />
+            <Button title="Search" onPress={() => handleSearch(search)} />
+          </View>
         </SafeAreaView>
       </LinearGradient>
     </View>
@@ -25,5 +46,18 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: colors.primary.dark,
     padding: 20,
+  },
+  coinSearchContainer: {
+    borderRadius: 10,
+    borderWidth: 0.3,
+    borderColor: colors.primary.light,
+    padding: 5,
+    margin: 10,
+    flexDirection: 'row',
+  },
+  input: {
+    flex: 1,
+    paddingLeft: 10,
+    color: colors.primary.light,
   },
 });
