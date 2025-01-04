2fbcba759c658137ac42a746cd13eaf543d96ac4
anilyavas <anil.yavas99@gmail.com>
2025-01-04 16:48:39 +0300

    Basic UI of searched Coin

--- app/(home)/investments.tsx
+++ app/(home)/investments.tsx
@@ -4,11 +4,12 @@ import { View, Text, StyleSheet, SafeAreaView, ActivityIndicator, Button } from 
 import { TextInput } from 'react-native-gesture-handler';
 
 import { colors } from '~/constants/colors';
-import { useCoinStore } from '~/store/store';
+import { Coin, useCoinStore } from '~/store/store';
 
 export default function InvestmentsScreen() {
   const { coins, loading, error, fetchCoins } = useCoinStore();
   const [search, setSearch] = useState('');
+  const [coin, setCoin] = useState<Coin>();
 
   useEffect(() => {
     fetchCoins();
@@ -20,8 +21,10 @@ export default function InvestmentsScreen() {
         coin.name.toLowerCase() === search.toLowerCase() ||
         coin.symbol.toLowerCase() === search.toLowerCase()
     );
-    return searchedCoin;
+    setCoin(searchedCoin);
   };
+
+  const addToWallet = () => {};
 
   if (loading) return <ActivityIndicator />;
   if (error) return <Text style={{ color: 'red', fontSize: 20, fontWeight: 'bold' }}>{error}</Text>;
@@ -43,6 +46,20 @@ export default function InvestmentsScreen() {
             <Button title="Search" onPress={() => handleSearch(search)} />
           </View>
           {/* Searched coin result */}
+          {coin && (
+            <View style={styles.coinContainer}>
+              <Text style={styles.coinText}>{coin.symbol}</Text>
+              <Text style={styles.coinText}>{coin.name}</Text>
+              <Text style={styles.coinText}>$ {coin.price_usd.toFixed(4)}</Text>
+              <Text
+                style={{
+                  color: coin.change_24h > 0 ? colors.secondary.main : colors.secondary.accent,
+                }}>
+                % {coin.change_24h.toFixed(4)}
+              </Text>
+              <Button title="Add" onPress={addToWallet} />
+            </View>
+          )}
         </SafeAreaView>
       </LinearGradient>
     </View>
@@ -71,5 +88,20 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingLeft: 10,
     color: colors.primary.light,
+  },
+  coinContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 10,
+    margin: 10,
+    borderWidth: 0.5,
+    borderColor: colors.primary.dark,
+    borderRadius: 10,
+  },
+  coinText: {
+    color: colors.primary.light,
+    fontSize: 16,
+    fontWeight: 'semibold',
   },
 });
