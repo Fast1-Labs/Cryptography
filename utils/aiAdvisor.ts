1a75ab1bab8a741eec33dbd9aa94b3911344522e
anilyavas <anil.yavas99@gmail.com>
2025-02-12 13:21:08 +0300

    Base of ai assistant

---
+++ utils/aiAdvisor.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const apiKey = process.env.OPENAI_API_KEY;
+const assistantID = process.env.OPENAI_ASSISTANT_ID;
+
+export const askCryptoAdvisor = async (query: string) => {
+  try {
+  } catch (error) {
+    console.log('Error while getting response ', error);
+  }
+};
